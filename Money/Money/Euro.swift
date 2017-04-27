67ed242eba967824efbcab4f1a6c1dc9ba6e00e0
Eugenio Barquin <eu.barquin@me.com>
2017-04-27 10:30:45 +0200

    testCanCreateEuro passed

---
+++ Money/Money/Euro.swift
@@ -0,0 +1,13 @@
+//
+//  Euro.swift
+//  Money
+//
+//  Created by Eugenio Barquín on 27/4/17.
+//  Copyright © 2017 Eugenio Barquín. All rights reserved.
+//
+
+import Foundation
+
+struct Euro {
+    
+}
