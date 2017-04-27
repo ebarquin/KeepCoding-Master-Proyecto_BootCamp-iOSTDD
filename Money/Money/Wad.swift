2c84f1d08b40ab11f21b48c28d6048e386c5321b
Eugenio Barquin <eu.barquin@me.com>
2017-04-27 13:56:18 +0200

    TestCanCreateWad passed

--- Money/Money/Wad.swift
+++ Money/Money/Wad.swift
@@ -8,6 +8,33 @@
 
 import Foundation
 
-struct Wad {
+typealias Bills = [Bill]
+
+struct Wad{
+    var _bills = Bills()
     
+    var billCount : Int {
+        get{
+            return _bills.count
+        }
+    }
+}
+
+extension Wad : Money{
+    
+    init(amount: Int, currency: Currency){
+        _bills.append(Bill(amount: amount, currency: currency))
+    }
+    
+    func times(_ n:Int)->Wad{
+        return self
+    }
+    
+    func plus(_ addend: Wad)-> Wad{
+        return self
+    }
+    
+    func reduced(to: Currency, broker: Broker) throws -> Wad{
+        return self
+    }
 }
